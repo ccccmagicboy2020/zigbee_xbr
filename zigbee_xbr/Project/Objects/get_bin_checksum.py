7496b827f25ae2dba6668ae857302293aae84dba
cccc <cuiwei_cv@163.com>
2021-02-04 16:36:54 +0800

    update ota bin file encode script

--- zigbee_xbr/Project/Objects/get_bin_checksum.py
+++ zigbee_xbr/Project/Objects/get_bin_checksum.py
@@ -1,7 +1,16 @@
-sum = 0;
-with open("HC-MCU-XBR.bin","rb") as f:
-    while True:
-            current_byte = f.read(1)
-            sum += current_byte[0]
-            print(hex(current_byte[0]))
-            print(hex(sum))
+sum = 0
+key = 0xcc
+byte_list = []
+with open("HC-MCU-XBR.bin", "rb") as f1:
+    byte_list = list(f1.read())
+    for i in range(len(byte_list)):
+        byte_list[i] ^= key
+    print(byte_list)
+    
+with open("HC-MCU-XBR_ota.bin","wb") as f2:
+    f2.write(bytearray(byte_list))
+    for x in byte_list:
+        sum += x
+    print(hex(sum))
+
+
